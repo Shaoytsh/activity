458f6fdeb1089adbe71277a6056e791811fff2ff
albert <shyttsyj@126.com>
2021-11-19 15:07:10 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ echo  '查看结果'
 ls docs/
 
 echo '移动到tlactivity_gitee'
-rm -rf ../activity_gitee/docs/
-cp -r docs/ ../activity_gitee/docs/
+rm -rf ../tlactivity_gitee/docs/
+cp -r docs/ ../tlactivity_gitee/docs/
 
 # echo '移动到tlactivity_github'
 # rm -rf ../tlactivity_github/docs/
